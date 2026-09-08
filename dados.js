ffba7a8a3cb141b88e1d0af3588ea09cd4e26346
luisleiteflash <luis.leite@move3.com.br>
2026-09-08 13:30:07 -0300

    Atualiza dados.js - 08/09/2026 13:30

--- dados.js
+++ dados.js
@@ -1,6 +1,6 @@
 /* gerado automaticamente a partir da planilha - nao editar a mao */
 window.PULSO = {
-  atualizado: "08/09/2026 13:15",
+  atualizado: "08/09/2026 13:30",
   arquivo: "Pulso B2B - painel v2 (1).xlsx",
   clientes: ["Casas Bahia", "Simpress", "Amazon", "Amazon Transferência", "Magalu", "Amazon Hub", "Liquitudo", "Orient", "Mercado livre"],
   linhas: [
